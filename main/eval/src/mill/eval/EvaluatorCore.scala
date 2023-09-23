5e213daaa26a53a7c5407f876f9789619e0648ee
Tobias Roeser <le.petit.fou@web.de>
2023-09-23 17:07:58 +0200

    imports

--- main/eval/src/mill/eval/EvaluatorCore.scala
+++ main/eval/src/mill/eval/EvaluatorCore.scala
@@ -5,7 +5,7 @@ import mill.api.Strict.Agg
 import mill.api._
 import mill.define._
 import mill.eval.Evaluator.TaskResult
-import mill.util._
+import mill.util.{ColorLogger, MultiBiMap, PrefixLogger}
 
 import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger}
 import scala.collection.mutable
